f3ca6341036df6312840aa068ef83b6314da0162
pcaiozzo <paigecaiozzo@gmail.com>
2022-04-21 12:37:30 -0400

    made prompts when button is clicked and generates password

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,62 @@
-// Assignment code here
+//Password generator will display password of 8-128 characters
 
+// Assignment code here
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+//Variables
+var specialCharacters = "!@#$%^&*();:<>+=-";
+var numberCharacters = "0123456789";
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
+//FUNCTIONS
+// Function to prompt password criteria
+function generatePassword() {
+  var password = "";
+  var passwordCharset = "";
+
+  // Prompt for password/ invalid if not between 8-128 characters
+  var passwordLength = window.prompt(" How long do you want your password to be? (between 8-128 characters)");
+  
+  if (Number(passwordLength < 8) | Number(passwordLength > 128) | isNaN(passwordLength)) {
+    window.alert("Your passowrd is not a valid lenght! (8-128)");
+    return;
+  }
+
+  // Prompt asking character types (numbers, lower Case, upper case, and special characters)
+  var characterSetChoice1 = window.confirm("Would you like to include numbers?");
+  if (characterSetChoice1) {passwordCharset += numberCharacters; }
+
+  var characterSetChoice2 = window.confirm("Would you like to include lowercase letters?");
+  if (characterSetChoice2) {passwordCharset += lowercaseCharacters; }
+
+  var characterSetChoice3 = window.confirm("Would you like to include upper case letters?");
+  if (characterSetChoice3) {passwordCharset += uppercaseCharacters; }
+
+  var characterSetChoice4 = window.confirm("Would you like to include special characters?");
+  if (characterSetChoice4) {passwordCharset += specialCharacters; }
+
+  // Statment incase user does not select anything at all
+  if (passwordCharset === "") {
+    window.alert("You must choose at leaset one option.");
+  }
+
+  //Stating password needs to be between 8-128 characters
+  if(passwordLength >=8 && passwordLength <=128) {
+    for(var i = 0; i < passwordLength; i++) {
+      password += passwordCharset.charAt(Math.floor(Math.random() * passwordCharset.length));
+    };
+    return password;
+  };
+};
+
+
+
+//Function for password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
